74c16c3bcb4a60626d0a948129e5b2d25821a2d8
saeed <saeedsaf@gmail.com>
2023-01-14 16:14:04 +0330

    initial commit

---
+++ lib-main-entities/src/main/java/io/github/farbod2u/libmainentities/LibMainEntitiesApplication.java
@@ -0,0 +1,13 @@
+package io.github.farbod2u.libmainentities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibMainEntitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibMainEntitiesApplication.class, args);
+    }
+
+}

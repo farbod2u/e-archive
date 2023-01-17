190025262cf2ba3ac7a46bc03043b7855a929e19
saeed <saeedsaf@gmail.com>
2023-01-17 18:06:04 +0330

    add package scanning

--- branch-service/src/main/java/io/github/farbod2u/branchservice/BranchServiceApplication.java
+++ branch-service/src/main/java/io/github/farbod2u/branchservice/BranchServiceApplication.java
@@ -2,7 +2,15 @@ package io.github.farbod2u.branchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {
+        "io.github.farbod2u",
+        "io.github.farbod2u.branchservice",
+        "io.github.farbod2u.exception.handler"
+})
+@EntityScan("io.github.farbod2u.*")
 @SpringBootApplication
 public class BranchServiceApplication {
 
